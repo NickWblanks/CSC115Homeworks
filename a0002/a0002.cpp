3141c56491f3ec3480584ca697c598ebd0f687f1
Wilk, Nicholas <nicholas.wilk@mines.sdsmt.edu>
2022-01-31 15:54:08 -0700

    a0002 - [ci skip] - finished wind chill function

--- a0002/a0002.cpp
+++ a0002/a0002.cpp
@@ -3,7 +3,7 @@
 int wind_chill( int Atemp, int wind )
 {
     
-    return int ( 35.74 + .6215 * Atemp - 35.75 * pow( wind, .16) + .4275 * Atemp * pow(wind, .16));
+    return int ( 35.74 + (.6215 * Atemp) - (35.75 * pow( wind, .16)) + (.4275 * Atemp * pow(wind, .16)));
     
 }
 
--- a0002/a0002Tests.cpp
+++ a0002/a0002Tests.cpp
@@ -2,7 +2,7 @@
 #include "..\\catch.hpp"
 #include "a0002.h"
 
-TEST_CASE( "wind_chil - testing to see if function works, Atemp - 32, wind - 15")
+TEST_CASE( "wind_chill - testing to see if function works, Atemp - 32, wind - 15")
 {
     int Atemp = 32;
     int wind = 15;
@@ -11,3 +11,40 @@ TEST_CASE( "wind_chil - testing to see if function works, Atemp - 32, wind - 15"
     new_temp = wind_chill( Atemp, wind);
     REQUIRE( new_temp == 21);
 }
+
+
+TEST_CASE( "wind_chill - testing negative temp, Atemp(-15), wind - 20")
+{
+    int Atemp = -15;
+    int wind = 20;
+    int new_temp;
+    
+    new_temp = wind_chill( Atemp, wind);
+    REQUIRE( new_temp == -41);
+}
+
+TEST_CASE( "wind_chill - testing high wind with high temp, Atemp(45), wind(55)")
+{
+    int Atemp = 45;
+    int wind = 55;
+    int new_temp;
+    
+    new_temp = wind_chill( Atemp, wind);
+    REQUIRE( new_temp == 32);
+}
+
+
+TEST_CASE( "wind_chill - testing high temp low wind, Atemp(49), wind(4)")
+{
+    int Atemp = 49;
+    int wind = 4;
+    int new_temp;
+    
+    new_temp = wind_chill( Atemp, wind);
+    REQUIRE( new_temp == 47);
+}
+
+
+
+
+    
