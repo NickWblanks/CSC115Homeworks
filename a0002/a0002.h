6e6a4e4b384d52cc878452682193ac66c4fcdb7f
Wilk, Nicholas <nicholas.wilk@mines.sdsmt.edu>
2022-02-04 11:53:30 -0700

    a0002 -[ci test] - added header

--- a0002/a0002.h
+++ a0002/a0002.h
@@ -1,6 +1,19 @@
 #pragma once
 #include <cmath>
+#include <string>
+
 
 using namespace std;
 
+
+
 int wind_chill( int Atemp, int wind );
+
+
+
+
+string windChillWarning( int Atemp, int wind );
+
+
+
+
