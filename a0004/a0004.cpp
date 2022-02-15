8dff6b702e8f9acb43c5018b9d04080a46fd8a71
Wilk, Nicholas <nicholas.wilk@mines.sdsmt.edu>
2022-02-14 18:36:54 -0700

    a0004 - [ci skip] - finished isPrime

--- a0004/a0004.cpp
+++ a0004/a0004.cpp
@@ -3,9 +3,35 @@
 
 bool isPrime( int num)
 {
-   num = 2;
-   return true;
+   int i;
+   i = 2;
+   
+   if( num < 2)
+   {
+       return false;
+   }
+   while( i <= 9)
+   {
+       if( num % i == 0)
+       {
+           if( num == 2)
+           {
+               i++;
+               return true;
+           }
+           
+           i++;
+           return false;
+           
+       }
+       
+       i++;
+       return true;
+       
+   }   
+   return false;
 }
+
 
     
     
--- a0004/a0004Tests.cpp
+++ a0004/a0004Tests.cpp
@@ -6,9 +6,41 @@
 TEST_CASE( "testing number 2, is prime")
 {
     int num = 2;
-    bool result;
-       
-    
-    result = isPrime( 2);
+    bool result;    
+    result = isPrime( num);
     REQUIRE( result == true);
+}
+
+TEST_CASE( "testing numer 3, is prime")
+{
+    int num = 3;
+    bool result;
+    result = isPrime( num);
+    REQUIRE( result == true);
+}
+
+TEST_CASE( "Testing number 4, not prime")
+{
+    int num = 4;
+    bool result;    
+    result = isPrime( num);
+    REQUIRE( result == false);
+}
+
+
+TEST_CASE( "Testing larger number 45, is not prime")
+{
+    int num = 45;
+    bool result;
+    result = isPrime( num);
+    REQUIRE( result == true);
+}
+
+
+TEST_CASE( "Testing 1, is not prime")
+{
+    int num = 1;
+    bool result;
+    result = isPrime( num);
+    REQUIRE( result == false);
 }
